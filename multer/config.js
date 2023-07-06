4c97b6702f21f0119103668a91309f595a71d6d5
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2023-07-06 22:45:25 +0300

    use path module

--- multer/config.js
+++ multer/config.js
@@ -5,9 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.fileStorage = void 0;
 const multer_1 = __importDefault(require("multer"));
+const path = require('path');
 exports.fileStorage = multer_1.default.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, __dirname + '/images/');
+        cb(null, path.join(__dirname + 'images'));
     },
     filename: (_req, file, cb) => {
         //console.log(file);
